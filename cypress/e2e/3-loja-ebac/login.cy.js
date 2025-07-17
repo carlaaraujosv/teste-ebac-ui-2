e7eb3ff7eea45b52fc6abc0199eb6da958fbac4e
Carla Araujo <carlaaraujo.qa@gmail.com>
2025-07-17 12:07:52 -0300

    Primeiro teste automatizado

---
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -0,0 +1,18 @@
+/// <reference types="cypress"/>
+
+describe('Funcionalidade: Login', () => {
+
+    it('Deve fazer login com sucesso', () => {
+        cy.visit ('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.get('#username'). type ('carla.teste@teste.com.br')
+        cy.get('#password'). type ('teste@1234')
+        cy.get('.woocommerce-form > .button') . click()
+
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)') . should ('contain', 'Olá, carla.teste (não é carla.teste? Sair)')
+
+    })
+
+
+
+
+})
